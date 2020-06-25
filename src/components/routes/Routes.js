56896a0c84f1ee9934f6fc877db396a9d1bb8508
JtawRobot <jeffersonjtaw@gmail.com>
2020-06-25 11:23:37 +0800

    error fix merge error

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-<<<<<<< HEAD:src/components/routes/Routes.js
-import Purchaserequestlist from '../purchaserequest/Purchaserequestlist';
-import Vendors from '../vendors/Vendors';
+import PurchaseRequestList from '../../containers/purchaserequest/PurchaseRequestList';
+import Vendors from '../../containers/vendors/Vendors';
 import Homepage from '../homepage/Homepage';
-=======
-import PurchaseRequestList from '../../containers/PurchaseRequest/PurchaseRequestList';
-import Vendors from '../../containers/Vendors/Vendors';
-import Homepage from '../Homepage/Homepage';
->>>>>>> 8f0c5f3199d651bf7cb65ff862eb3767a0ba2eb7:src/Components/Routes/Routes.js
 
 const Routes = () => {
   const routes = (
