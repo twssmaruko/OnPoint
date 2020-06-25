8f0c5f3199d651bf7cb65ff862eb3767a0ba2eb7
twssmaruko <pneumonia_129@yahoo.com>
2020-06-25 10:55:07 +0800

    Changed Folder Structure. Changed Name Structures.

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Purchaserequestlist from '../Purchaserequest/Purchaserequestlist';
-import Vendors from '../Vendors/Vendors';
+import PurchaseRequestList from '../../containers/PurchaseRequest/PurchaseRequestList';
+import Vendors from '../../containers/Vendors/Vendors';
 import Homepage from '../Homepage/Homepage';
 
 const Routes = () => {
   const routes = (
     <Switch>
-      <Route path="/purchaserequest" exact component={Purchaserequestlist} />
+      <Route path="/purchaserequest" exact component={PurchaseRequestList} />
       <Route path="/vendors" exact component={Vendors} />
       <Route path="/" exact component={Homepage} />
       <Redirect to="/" />
