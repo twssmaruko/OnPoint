1e07cc829055074d09a955fcf4c650c309f07938
JtawRobot <jeffersonjtaw@gmail.com>
2020-07-22 11:04:09 +0800

    added code splitting

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
-import PurchaseRequests from '../../containers/PurchaseRequests/PurchaseRequests';
-import Vendors from '../../containers/Vendors/Vendors';
 import Homepage from '../homepage/Homepage';
 import Auth from '../../containers/Auth/Auth';
-import Products from '../../containers/Products/Products';
-import Projects from '../../containers/Projects/Projects';
-import PurchaseOrders from '../../containers/PurchaseOrders/PurchaseOrders';
+// import PurchaseRequests from '../../containers/PurchaseRequests/PurchaseRequests';
+// import Vendors from '../../containers/Vendors/Vendors';
+// import Products from '../../containers/Products/Products';
+// import Projects from '../../containers/Projects/Projects';
+// import PurchaseOrders from '../../containers/PurchaseOrders/PurchaseOrders';
+
+const Vendors = lazy(() => import('../../containers/Vendors/Vendors'));
+const Products = lazy(() => import('../../containers/Products/Products'));
+const PurchaseRequests = lazy(() =>
+  import('../../containers/PurchaseRequests/PurchaseRequests'));
+const PurchaseOrders = lazy(() =>
+  import('../../containers/PurchaseOrders/PurchaseOrders'));
+const Projects = lazy(() => import('../../containers/Projects/Projects'))
+
+// const Checkout = React.lazy(() => {
+//     return import('./containers/Checkout/Checkout');
+//   });
 
 const Routes = () => {
-    const routes =
+  const routes =
     <Switch>
-        <Route path="/purchaseorders" exact component={PurchaseOrders} />
-        <Route path="/purchaserequest" exact component={PurchaseRequests} />
-        <Route path="/auth" exact component={Auth} />
-        <Route path="/vendors" exact component={Vendors} />
-        <Route path="/projects" exact component={Projects} />
-        <Route path="/" exact component={Homepage} />
-        <Route path="/products" exact component={Products} />
-        <Redirect to="/" />
+      <Route path="/purchaseorders" exact render={(props) => <PurchaseOrders {...props}/>} />
+      <Route path="/purchaserequest" exact render={(props) => <PurchaseRequests {...props}/>} />
+      <Route path="/auth" exact component={Auth} />
+      <Route path="/vendors" exact render={(props) => <Vendors {...props}/>} />
+      <Route path="/projects" exact render={(props) => <Projects {...props}/>}/>
+      <Route path="/" exact component={Homepage} />
+      <Route path="/products" exact render={(props) => <Products {...props}/>} />
+      <Redirect to="/" />
     </Switch>;
-    return (
-        <>
-            {routes}
-        </>
-    );
+  return (
+    <Suspense fallback={<div>loading....</div>}>
+      {routes}
+    </Suspense>
+  );
 };
 
 export default Routes;
