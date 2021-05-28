948f95220f98adfe1912ccf6e9a3bdcd16cbf0e4
twssmaruko <pneumonia_129@yahoo.com>
2021-05-28 11:24:18 +0800

    test commit

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
 const Pool = require("pg").Pool;
 
+const deploymentServer = "192.168.254.170";
+const localServer = "localhost";
 const pool = new Pool({
     user: "my_user",
     password: "root",
-    host: "localhost",
+    host: localServer,
     port: 5432,
     database: "onpoint_database"
 });
